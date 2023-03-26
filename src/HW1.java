ee3036678c93fca92e5f00d094c1714dcb10ab3d
zhenyaitqa <zhenya.itqa@gmail.com>
2023-03-26 21:36:48 +0300

    commit hw1

--- src/HW1.java
+++ src/HW1.java
@@ -1,12 +1,12 @@
 class HW1 {
     int year = 2023;
 
-    public static int MYMETHOD(int a, int b){
+    public static int sumOfTwoNumbers(int a, int b){
         int sum = a + b;
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(MYMETHOD(7,11));
+        System.out.println(sumOfTwoNumbers(7, 11));
     }
 }
